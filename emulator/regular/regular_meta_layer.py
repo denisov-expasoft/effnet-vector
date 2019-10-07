5b8cbbebec83261f1ba1f791cecac3f90bdf86b2
a.denisov <a.denisov@expasoft.ru>
2019-10-07 17:30:36 +0700

    Added support for swish and sigmoid functions to the regular (float) model

--- emulator/regular/regular_meta_layer.py
+++ emulator/regular/regular_meta_layer.py
@@ -8,7 +8,7 @@ from emulator.common import LayerConfigurationParameters as Lcp
 from emulator.layers import BaseMetaLayer
 from emulator.layers import LAYERS_REGISTRY
 
-_SUPPORTED_ACTIVATIONS = (None, 'relu')
+_SUPPORTED_ACTIVATIONS = (None, 'relu', 'swish', 'sigmoid')
 
 
 class RegularMetaLayer(BaseMetaLayer):
@@ -26,7 +26,16 @@ class RegularMetaLayer(BaseMetaLayer):
         super().__init__(layers_registry=LAYERS_REGISTRY, **layer_cfg)
 
     def _process_output_tensor(self, output_tensor: tf.Tensor) -> tf.Tensor:
+        if self._activation_function is None:
+            return output_tensor
+
         if self._activation_function == 'relu':
             return tf.nn.relu(output_tensor)
 
-        return output_tensor
+        if self._activation_function == 'sigmoid':
+            return tf.nn.sigmoid(output_tensor)
+
+        if self._activation_function == 'swish':
+            return tf.nn.swish(output_tensor)
+
+        raise NotImplementedError(f'Behaviour of "{self._activation_function}" is not implemented')
